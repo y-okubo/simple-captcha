f1f13120c3f3ffb53a13fbf51521a78d8a560ead
aiasfina <aiasfina@gmail.com>
2013-10-28 14:56:18 +0800

    support redis, rails 4, upgrade to 0.1.6

--- lib/simple_captcha/engine.rb
+++ lib/simple_captcha/engine.rb
@@ -5,12 +5,25 @@ require 'simple_captcha'
 module SimpleCaptcha
   class Engine < ::Rails::Engine
     config.before_initialize do
-      ActiveSupport.on_load :active_record do
-        ActiveRecord::Base.send(:include, SimpleCaptcha::ModelHelpers)
+      case SimpleCaptcha.store
+        when 'active_record'
+          require 'simple_captcha/active_record'
+          require 'simple_captcha/simple_captcha_data'
+
+          ActiveSupport.on_load :active_record do
+            ActiveRecord::Base.send(:include, SimpleCaptcha::ModelHelpers)
+          end
+        when 'mongoid'
+          require 'simple_captcha/simple_captcha_data_mongoid.rb'
+        when 'redis'
+          require 'simple_captcha/simple_captcha_data_redis.rb'
+        else
+          raise StandardError.new("Unknown store: #{SimpleCaptcha.store}")
       end
     end
 
     config.after_initialize do
+
       ActionView::Base.send(:include, SimpleCaptcha::ViewHelper)
       ActionView::Helpers::FormBuilder.send(:include, SimpleCaptcha::FormBuilder)
 
