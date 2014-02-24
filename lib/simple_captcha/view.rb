1298b4cbab1a9a5ba1f62dc1d001b84c234494ad
TEMKiN <aa.temkin@gmail.com>
2014-02-24 13:48:28 +0700

    change :object to :raw_object

--- lib/simple_captcha/view.rb
+++ lib/simple_captcha/view.rb
@@ -50,7 +50,7 @@ module SimpleCaptcha #:nodoc
          :image => simple_captcha_image(key, options),
          :label => options[:label] || I18n.t('simple_captcha.label'),
          :field => simple_captcha_field(options),
-         :object => options[:object]
+         :raw_object => options[:raw_object] || options[:object]
          }
 
       render :partial => 'simple_captcha/simple_captcha', :locals => { :simple_captcha_options => defaults }
